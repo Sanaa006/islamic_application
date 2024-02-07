804d59f256b40a468df42fc16e34620078fc7a78
sanaa khalaf <sanaakhalf006@gmail.com>
2024-02-06 21:47:14 -0800

    added feature screen quran tap

--- lib/my_theme.dart
+++ lib/my_theme.dart
@@ -4,6 +4,7 @@ class MyTheme {
   /// colors, Theme Light Mode , theme Dark Mode
   static Color blackColor = Color(0xff242424);
   static Color primaryLight = Color(0xffB7935F);
+  static Color whiteColor = Color(0x7bffffff);
   static ThemeData lightMode = ThemeData(
     primaryColor: primaryLight,
     scaffoldBackgroundColor: Colors.transparent,
@@ -11,14 +12,26 @@ class MyTheme {
       selectedItemColor: blackColor,
     ),
     appBarTheme: AppBarTheme(
-      color: Colors.transparent,
-      centerTitle: true,
-      elevation: 0,
+        color: Colors.transparent,
+        centerTitle: true,
+        elevation: 0,
+        iconTheme: IconThemeData(
+            color: blackColor
+        )
     ),
     textTheme: TextTheme(
         titleLarge: TextStyle(
-      fontWeight: FontWeight.bold,
-      fontSize: 30,
-    )),
+          fontWeight: FontWeight.bold,
+          fontSize: 30,
+        ),
+        titleMedium: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.w500
+        ),
+        titleSmall: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.w400
+        )
+    ),
   );
 }
